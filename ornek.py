847ee1ff74d5dd231e630bc69268876b9a125e97
bgoren75 <59222914+bgoren75@users.noreply.github.com>
2019-12-31 15:48:27 +0300

    Create ornek.py

---
+++ ornek.py
@@ -0,0 +1,25 @@
+import sys
+print("burak")
+a = 0
+# print(a)
+while a < 5:
+    if a < 3:
+        print(a)
+    else:
+        print("burak")
+    a = a + 1
+print("done")
+
+while a < 10:
+    print(a)
+    a = a + 1
+
+print("deneme")
+for i in range(0, 3):
+    print(i)
+
+print("başka")
+count = 0
+while count < 5:
+    print(count)
+    count += 1  # This is the same as count = count + 1
